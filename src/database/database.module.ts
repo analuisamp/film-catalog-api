fd29a13ed5b77ef461bfca5222b632a1486fae74
analuisamp <dev.06@controlit.com.br>
2024-05-04 20:34:24 -0300

    trying to correct

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,7 +7,6 @@ import { DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  url: 'postgres://postgres:mks@localhost:5432/movie',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
