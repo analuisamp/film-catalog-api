6efa3636a57792673dae1dcbe9c58d3526e5cc02
analuisamp <dev.06@controlit.com.br>
2024-05-04 22:07:02 -0300

    trying to correct

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -17,12 +17,24 @@ export const dataSourceOptions: DataSourceOptions = {
   synchronize: false,
 }
 
-const environment = process.env.NODE_ENV || "development";
-const knex = require("knex");
-const knexfile = require("./knexfile");
+const { Client } = require('pg');
 
-const db = knex(knexfile[environment]);
-module.exports = db;
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 @Module({
   imports: [TypeOrmModule.forRootAsync({
